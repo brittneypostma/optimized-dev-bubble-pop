d6bdb3b5f395d12dbd90c80e1a61ff15e88b0816
brittneypostma <bdesignwebdev@gmail.com>
2022-06-06 17:09:07 -0400

    lesson 1

---
+++ 02-transitions/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import './lib/styles.css'
+	import Bubbles from './lib/components/bubbles/Bubbles.svelte'
+</script>
+
+<main>
+	<h1>Welcome to Bubble Pop</h1>
+	<Bubbles />
+</main>
+
+<style>
+	main {
+		display: grid;
+		min-height: 100vh;
+		place-items: center;
+		place-content: center;
+		gap: var(--size-4);
+	}
+</style>
