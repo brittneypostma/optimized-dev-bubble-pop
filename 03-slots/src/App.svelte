e46b52fab87fe67d2f4456943820d60c9d97a321
brittneypostma <bdesignwebdev@gmail.com>
2022-06-07 14:22:56 -0400

    small changes

--- 03-slots/src/App.svelte
+++ 03-slots/src/App.svelte
@@ -1,11 +1,13 @@
 <script>
 	import './lib/styles.css'
 	import Bubbles from './lib/components/bubbles/Bubbles.svelte'
+	import GameBar from './lib/components/GameBar.svelte'
 </script>
 
 <main>
 	<h1>Welcome to Bubble Pop</h1>
 	<Bubbles />
+	<GameBar />
 </main>
 
 <style>
--- 04-stores/src/lib/stores.js
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store'
-
-export const session = writable(false)
-
-export const score = writable(0)
-
-export const finalScore = writable(10)
-
-export const totalBubbles = writable(50)
